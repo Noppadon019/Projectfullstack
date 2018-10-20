fac8091f3dd293c51e0f045894500963331ca410
Noppadon019 <kotan.nam007@hotmail.com>
2018-10-20 12:31:24 +0700

    ttest

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ app.post('/product/update',function (req, res){
      var title = req.body.title;
      var price = req.body.price;
      var sql = `update product set title = '${title}',price = '${price}' where id = ${id}`;
-     res.send(sql)
+     
      //do.none
      db.none(sql);
      console.log('UPDATE:'+ sql);
