e970bbf1585c75191042446159687aebeb3c8d8d
Noppadon019 <kotan.nam007@hotmail.com>
2018-10-20 11:47:03 +0700

    update server/addnew

--- server.js
+++ server.js
@@ -108,6 +108,31 @@ app.get('/users', function (req, res) {
         })
 });
 
+//Add New Product
+app.get('/insert',function (req, res) {
+    res.render('pages/insert'); 
+})
+app.post('/products/insert', function (req, res) {
+    var id = req.body.id;
+    var title = req.body.title;
+    var price = req.body.price;
+    var sql = `INSERT INTO products (id,title,price)
+    VALUES ('${id}', '${title}', '${price}')`;
+    //db.none
+    console.log('UPDATE:' + sql);
+    db.any(sql)
+        .then(function (data) {
+            console.log('DATA:' + data);
+            res.redirect('/products')
+        })
+
+        .catch(function (error) {
+            console.log('ERROR:' + error);
+        })
+});
+
+
+
 
 
 var port = process.env.PORT || 8080;
